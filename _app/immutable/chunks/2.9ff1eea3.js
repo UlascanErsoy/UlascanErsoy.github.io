12569cb9f91aff1b6e9daa11ac3e83b598164c4d
Muhammed Ersoy <ersoy.ulascan@gmail.com>
2023-03-05 09:36:51 -0600

    Updates

--- _app/immutable/chunks/2.9ff1eea3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.8a8e221a.js";export{t as component};
--- _app/immutable/chunks/3.d9733cf2.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/hanzi-page.svelte.b1bcbfe8.js";export{t as component};
