00725b565e0d462a66661f3cc6191d798737d9d9
Muhammed Ersoy <ersoy.ulascan@gmail.com>
2023-03-07 23:59:34 -0600

    fixed some margins

---
+++ _app/immutable/chunks/1.ea859ec8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.7b65ad0b.js";export{t as component};
---
+++ _app/immutable/chunks/2.1454bb6a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.81488547.js";export{t as component};
