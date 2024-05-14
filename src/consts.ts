cbf5e97599e705c7d4df70db442c95a2e8c29a57
UlascanErsoy <ulas.742@gmail.com>
2024-05-14 15:30:08 -0500

    first

---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Codes & Chats';
+export const SITE_DESCRIPTION = 'Welcome to Codes & Chats Blog'; 
+export const TOP_LEVEL_LINKS = [
+                    {name:"Home", href:"/"},
+                    {name:"Blog", href:"/blog"},
+                    {name:"About", href:"/about"}
+                    ];
